e1c8e468df261fb462f93be6f2bfc76e90682992
gapeee <gsputra20@gmail.com>
2022-06-04 23:43:54 +0700

    Adding Hero Section & About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#10b981',
+        secondary: '#9ca3af',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+
+    },
   },
   plugins: [],
 }
